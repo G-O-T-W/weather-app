cc69dcad2a958fcd910824f82fdef105c4c05f9e
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2025-11-06 21:34:31 +0530

    Display Weather Data in DOM

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -14,12 +14,41 @@ export default class UI {
     if(sunset != "")
       sunset = DateTime.fromISO(sunset).toFormat("hh:mm a"); //format
 
+    // Console logging
     console.log(location, time);
     console.log("Current Temperature: ", currentTemp);
     console.log("Humidity: ", humidity);
     console.log("AQI: ", aqi);
     console.log("Sunrise: ", sunrise);
     console.log("Sunset: ", sunset);
-    // console.log(weatherData);
+    
+    // Change DOM
+    const h1 = document.querySelector(".heading h1");
+    h1.textContent = `${location}, ${time}`;
+    
+    const cardTemp = document.querySelector(".card-temperature");
+    const paraTemp = document.createElement("p");
+    paraTemp.textContent = `${currentTemp} C`;
+    cardTemp.appendChild(paraTemp);
+    
+    const cardAQI = document.querySelector(".card-aqi");
+    const paraAQI = document.createElement("p");
+    paraAQI.textContent = `${aqi}`;
+    cardAQI.appendChild(paraAQI);
+
+    const cardHumidity = document.querySelector(".card-humidity");
+    const paraHumidity = document.createElement("p");
+    paraHumidity.textContent = `${humidity} %`;
+    cardHumidity.appendChild(paraHumidity);
+
+    const cardDaylight = document.querySelector(".card-daylight");
+    const paraSunrise = document.createElement("p");
+    const paraSunset = document.createElement("p");
+    paraSunrise.textContent = `${sunrise}`;
+    paraSunset.textContent = `${sunset}`;
+    cardDaylight.appendChild(paraSunrise);
+    cardDaylight.appendChild(paraSunset);
+
+  
   }
 }
